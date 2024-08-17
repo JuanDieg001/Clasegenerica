bb5be101a1ed510ad4f2651b8839465dcaaad4c4
gitLove <anthonyjosue2000@gmail.com>
2024-08-17 16:47:38 -0500

    a

--- main.py
+++ main.py
@@ -2,11 +2,15 @@ from accessData.conexionORM import Database  # Importar la clase Database
 from accessData.entities.estudianteModel import BaseDeDatos
 from dominio.entities.modelsOrm.estudiante_dto import EstudianteBase  # Importar el DTO EstudianteDto
 from servicio.estudiante_service import EstudianteService
+from servicio.genericService import GenericRepository
+from accessData.entities.estudianteModel import Estudiante
 
 DATABASE_URL = "mysql+mysqlconnector://root:admin@localhost:3307/instituto"
 
 def main():
-    estudiante_service = EstudianteService(db)
+    # estudiante_service = EstudianteService(db)
+
+    student_repository = GenericRepository[Estudiante](Estudiante, db)
 
     while True:
         print("\n--- MENU ---")
@@ -27,12 +31,15 @@ def main():
             matricula = input("Ingrese la matricula: ")
             carrera = input("Ingrese la carrera: ")
 
-            student_dto = EstudianteBase(None, nombre, apellido, edad, mail, matricula, carrera)
-            estudiante_service.create_student(student_dto)
-            print(vars(student_dto))
+            student = EstudianteBase(None, nombre, apellido, edad, mail, matricula, carrera)
+            # estudiante_service.create_student(student_dto)
+            student_repository.create(nombre=student.nombre, apellido=student.apellido, edad=student.edad, mail=student.mail, matricula=student.matricula, carrera=student.carrera)
+
+            print(vars(student))
 
         elif opcion == "2":
-            estudiante = estudiante_service.get_student_all()
+            # estudiante = estudiante_service.get_student_all()
+            estudiante = student_repository.getAll()
             for estudiante in estudiante:
                 print(f"ID: {estudiante.id}, Nombre: {estudiante.nombre}, Apellido: {estudiante.apellido}, Edad: {estudiante.edad}, Mail: {estudiante.mail}, Matrícula: {estudiante.matricula}, Carrera: {estudiante.carrera}")
             
@@ -41,7 +48,8 @@ def main():
 
         elif opcion == "3":
             estudiante_id = int(input("Ingrese el ID del estudiante a consultar: "))
-            estudiante = estudiante_service.get_student(estudiante_id)
+            #estudiante = estudiante_service.get_student(estudiante_id)
+            estudiante = student_repository.get_student(estudiante_id)
             if estudiante:
                 print(f"ID: {estudiante.id}, Nombre: {estudiante.nombre}, Apellido: {estudiante.apellido}, Edad: {estudiante.edad}, Mail: {estudiante.mail}, Matrícula: {estudiante.matricula}, Carrera: {estudiante.carrera}")
             else:
@@ -56,16 +64,20 @@ def main():
             matricula = input("Ingrese la matricula: ")
             carrera = input("Ingrese la carrera: ")
             student = EstudianteBase(id_estudiante, nombre, apellido, edad, mail, matricula, carrera)
-            estudiante_service.update_student(student)
+            # estudiante_service.update_student(student)
+            estudiante = student_repository.update_student(student)
             print("Estudiante actualizado")
 
         elif opcion == "5":
             estudiante_id = int(input("Ingrese el ID del estudiante que desea eliminar: "))
-            estudiante_service.delete_student(estudiante_id)
+            # estudiante_service.delete_student(estudiante_id)
+            student_repository.delete_student(estudiante_id)
+
             print("Estudiante eliminado")
 
         elif opcion == "6":
-            estudiante_service.cerrarConexion()
+            # estudiante_service.cerrarConexion()
+            student_repository.cerrarConexion()
             print("¡Hasta luego!")
             break
 
